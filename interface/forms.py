aade5a5f1e4acff3594b141d8770842debe5a6b0
Liam Reeves <wesleyliam005@gmail.com>
2024-06-29 23:33:13 +0300

    designed contact section

--- interface/forms.py
+++ interface/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'style': 'width: 300px;'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'style': 'width: 300px;'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 300px; height: 200px;'}))
+    name = forms.CharField(max_length = 100, required = True, widget = forms.TextInput(attrs={'style': 'width: 250px; height: 50px;', 'placeholder': 'Name' }))
+
+    email = forms.EmailField(max_length = 200, required = True, widget = forms.EmailInput(attrs={'style': 'width: 250px; height: 50px;', 'placeholder': 'Email' }) )
+
+    message = forms.CharField(max_length=1500, required = False, widget = forms.Textarea(attrs={'style': 'width: 300px; height: 200px;', 'placeholder': 'Message' }))
 
--- interface/views.py
+++ interface/views.py
@@ -24,10 +24,15 @@ def services(request):
     return render(request, "services.html")
 
 def contact(request):
-    form = ContactForm()
+    # if this is a POST request we'll process the form data
+    form = ContactForm()  # i must make a declaration to show that the form is available for input
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        #if you are viewing this it took me two hours to figure this out
+
+        form = ContactForm(request.POST) 
+        # check whether it's valid:
         if form.is_valid():
+            # process the data in form.cleaned_data
             name= form.cleaned_data['name']
             email= form.cleaned_data['email']
             message= form.cleaned_data['message']
